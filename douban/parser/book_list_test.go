13712c4b65e90ee5aad3448394321137c39060ee
Flamingo <490493499@qq.com>
2023-06-07 08:23:51 +0800

    feat. 实现前端展示页面

--- douban/parser/book_list_test.go
+++ douban/parser/book_list_test.go
@@ -1,7 +1,6 @@
-package test
+package parser
 
 import (
-	"crawler_book/douban/parser"
 	"crawler_book/fetcher"
 	"testing"
 )
@@ -11,18 +10,13 @@ func TestParseBookList(t *testing.T) {
 	if err != nil {
 		panic(err)
 	}
-	result := parser.ParseBookList(contents)
+	result := ParseBookList(contents)
 
 	const resultSize = 14
 	expectedUrls := []string{
 		"https://book.douban.com/subject/36142067/",
 		"https://book.douban.com/subject/35407136/",
-		"https://book.douban.com/subject/36237507/",
-	}
-	expectedBooks := []string{
-		"动手学深度学习（PyTorch版）",
-		"你看起来好像……我爱你",
-		"BERT基础教程：Transformer大模型实战",
+		"https://book.douban.com/subject/30192800/",
 	}
 
 	if len(result.Requests) != resultSize {
@@ -31,15 +25,6 @@ func TestParseBookList(t *testing.T) {
 	for i, url := range expectedUrls {
 		if result.Requests[i].Url != url {
 			t.Errorf("expected url is %d: %s, but was %s", i, url, result.Requests[i].Url)
-		}
-	}
-
-	if len(result.Items) != resultSize {
-		t.Errorf("result should have %d requests, but had %d", resultSize, len(result.Items))
-	}
-	for i, book := range expectedBooks {
-		if result.Items[i].(string) != book {
-			t.Errorf("expected book is %d: %s, but was %s", i, book, result.Items[i].(string))
 		}
 	}
 }
